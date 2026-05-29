d5048c1e02ca65577e12b78a949c376d46fdbe9b
beerck7 <z.norbert0104@gmail.com>
2026-05-29 12:09:12 +0200

    Wprowadzenie autorskiego, bezramkowego paska tytulowego z plynna obsluga aero-snap i przyciskami sterowania

--- SubscriptionTracker/MainWindow.xaml.cs
+++ SubscriptionTracker/MainWindow.xaml.cs
@@ -9,5 +9,20 @@ namespace SubscriptionTracker
             InitializeComponent();
             DataContext = new ViewModels.MainViewModel();
         }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
